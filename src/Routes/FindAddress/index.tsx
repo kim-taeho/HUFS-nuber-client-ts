dce6ab583d00c26c829f99af51c12598551b59a9
kimtaeho <xxxxoghxxx.thkim@gmail.com>
2019-12-05 21:29:56 +0900

    Login화면

---
+++ src/Routes/FindAddress/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const R = () => <span>lalala</span>;
+
+export default R;
--- src/global-styles.ts
+++ src/global-styles.ts
@@ -20,4 +20,7 @@ createGlobalStyle`
     button{
         &:focus, &:active(outline:none)
     }
+    h1,h2,h3,h4,h5,h6{
+        font-family:'Maven Pro', sans-serif;
+    }
 `;
