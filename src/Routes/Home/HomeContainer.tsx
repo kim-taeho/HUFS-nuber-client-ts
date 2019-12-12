8819f6c1e6a9aec85e39f7bb033848be4943125b
kimtaeho <xxxxoghxxx.thkim@gmail.com>
2019-12-12 18:00:20 +0900

    to, from bounds 생성

--- src/Routes/Home/HomeContainer.tsx
+++ src/Routes/Home/HomeContainer.tsx
@@ -5,11 +5,19 @@ import { Query } from "react-apollo";
 import { userProfile } from "../../types/api";
 import { USER_PROFILE } from "../../sharedQueries";
 import ReactDOM from "react-dom";
+import { geoCode } from "../../mapHelpers";
+import { toast } from "react-toastify";
 
 interface IState {
     isMenuOpen: boolean;
     lat: number;
     lng: number;
+    toLat: number;
+    toLng: number;
+    toAddress: string;
+    distance: string;
+    duration?: string;
+    price?: number;
 
 }
 interface IProps extends RouteComponentProps<any> {
@@ -22,10 +30,18 @@ class HomeContainer extends React.Component<IProps, IState> {
     public mapRef: any;
     public map: google.maps.Map;
     public userMarker: google.maps.Marker;
+    public toMarker: google.maps.Marker;
+    public directions: google.maps.DirectionsRenderer;
     public state = {
         isMenuOpen: false,
         lat: 0,
-        lng: 0
+        lng: 0,
+        toLat: 0,
+        toLng: 0,
+        toAddress: "",
+        distance: "",
+        duration: undefined,
+        price: undefined
     };
     constructor(props) {
         super(props);
@@ -39,7 +55,7 @@ class HomeContainer extends React.Component<IProps, IState> {
     }
 
     public render() {
-        const { isMenuOpen } = this.state;
+        const { isMenuOpen, toAddress, price } = this.state;
         return (
             <ProfileQuery query={USER_PROFILE}>
                 {({ loading }) => (
@@ -47,6 +63,10 @@ class HomeContainer extends React.Component<IProps, IState> {
                         loading={loading}
                         isMenuOpen={isMenuOpen}
                         toggleMenu={this.toggleMenu}
+                        toAddress={toAddress}
+                        onInputChange={this.onInputChange}
+                        price={price}
+                        onAddressSubmit={this.onAddressSubmit}
                         mapRef={this.mapRef} />
                 )}
             </ProfileQuery>
@@ -78,8 +98,7 @@ class HomeContainer extends React.Component<IProps, IState> {
                 lng
             },
             disableDefaultUI: true,
-            minZoom: 8,
-            zoom: 11
+            zoom: 13
         };
         this.map = new maps.Map(mapNode, mapConfig);
         const userMarkerOptions: google.maps.MarkerOptions = {
@@ -95,6 +114,14 @@ class HomeContainer extends React.Component<IProps, IState> {
         this.userMarker = new maps.Marker(userMarkerOptions);
         this.userMarker.setMap(this.map);
     }
+    public onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const {
+            target: { name, value }
+        } = event;
+        this.setState({
+            [name]: value
+        } as any);
+    };
     public handleGeoWatchSuccess = (position: Position) => {
         const { coords: { latitude, longitude } } = position;
         this.userMarker.setPosition({ lat: latitude, lng: longitude });
@@ -105,6 +132,81 @@ class HomeContainer extends React.Component<IProps, IState> {
     }
     public handleGeoError = () => {
         console.log("No location");
+    }
+    public onAddressSubmit = async () => {
+        const { toAddress } = this.state;
+        const { google } = this.props;
+        const maps = google.maps;
+        const result = await geoCode(toAddress);
+        if (result !== false) {
+            const { lat, lng, formatted_address } = result;
+            if (this.toMarker) {
+                this.toMarker.setMap(null); // 이미 지정된 toMarker가 있다면 지워줌
+            }
+            const toMarkerOptions: google.maps.MarkerOptions = {
+                position: {
+                    lat,
+                    lng
+                }
+            };
+            this.toMarker = new maps.Marker(toMarkerOptions);
+            this.toMarker.setMap(this.map);
+            const bounds = new maps.LatLngBounds();
+            bounds.extend({ lat, lng }); // toAddress
+            bounds.extend({ lat: this.state.lat, lng: this.state.lng }); // User가 있는 장소
+            this.map.fitBounds(bounds);
+            this.setState({
+                toAddress: formatted_address,
+                toLng: lng,
+                toLat: lat
+            }, this.createPath); // setState이후에 createPath 실행
+        }
+    }
+    public createPath = () => {
+        const { toLat, toLng, lat, lng } = this.state;
+        if (this.directions) {
+            this.directions.setMap(null);
+        }
+        const renderOptions: google.maps.DirectionsRendererOptions = {
+            suppressMarkers: true,
+            polylineOptions: {
+                strokeColor: "#000"
+            }
+        }
+        this.directions = new google.maps.DirectionsRenderer(renderOptions);
+        const directionService: google.maps.DirectionsService = new google.maps.DirectionsService();
+        const to = new google.maps.LatLng(toLat, toLng);
+        const from = new google.maps.LatLng(lat, lng);
+        const directionOptions: google.maps.DirectionsRequest = {
+            destination: to,
+            origin: from,
+            travelMode: google.maps.TravelMode.TRANSIT
+        };
+        directionService.route(directionOptions, (result, status) => {
+            if (status === google.maps.DirectionsStatus.OK) {
+                const { routes } = result;
+                const {
+                    distance: { text: distance },
+                    duration: { text: duration }
+                } = routes[0].legs[0];
+                this.directions.setDirections(result);
+                this.directions.setMap(this.map);
+                this.setState({
+                    distance,
+                    duration
+                }, this.setPrice);
+            } else {
+                toast.error("There is no route there");
+            }
+        })
+    }
+    public setPrice = () => {
+        const { distance } = this.state;
+        if (distance) {
+            this.setState({
+                price: parseFloat(distance.replace(".", ",")) * 3
+            })
+        }
     }
 }
 
--- src/Routes/Home/HomePresenter.tsx
+++ src/Routes/Home/HomePresenter.tsx
@@ -3,10 +3,12 @@ import Helmet from "react-helmet";
 import styled from "../../typed-components";
 import Sidebar from "react-sidebar";
 import Menu from "../../Components/Menu";
+import Button from "../../Components/Button";
+import AddressBar from "../../Components/AddressBar";
 
 const Container = styled.div``;
 
-const Button = styled.button`
+const MenuButton = styled.button`
   appearance: none;
   padding: 10px;
   position: absolute;
@@ -22,6 +24,21 @@ const Button = styled.button`
   background-color: transparent;
 `;
 
+const ExtendedButton = styled(Button)`
+  position: absolute;
+  bottom: 50px;
+  left: 0;
+  right: 0;
+  margin: auto;
+  z-index: 10;
+  height: auto;
+  width: 80%;
+`;
+
+const RequestButton = styled(ExtendedButton)`
+    bottom: 250px;
+`;
+
 const Map = styled.div`
   position: absolute;
   height: 100%;
@@ -33,9 +50,13 @@ interface IProps {
     toggleMenu: () => void;
     loading: boolean;
     mapRef: any;
+    toAddress: string;
+    onAddressSubmit: () => void;
+    onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    price?: number;
 }
 
-const HomePresenter: React.SFC<IProps> = ({ mapRef, isMenuOpen, toggleMenu, loading }) => (
+const HomePresenter: React.SFC<IProps> = ({ mapRef, isMenuOpen, toggleMenu, loading, toAddress, onAddressSubmit, onInputChange, price }) => (
     <Container>
         <Helmet>
             <title>Home | HUFS CAR</title>
@@ -46,7 +67,10 @@ const HomePresenter: React.SFC<IProps> = ({ mapRef, isMenuOpen, toggleMenu, load
             onSetOpen={toggleMenu}
             styles={{ sidebar: { background: "white", width: "80%", zIndex: "10" } }}
         >
-            {!loading && <Button onClick={() => toggleMenu()}>|||</Button>}
+            {!loading && <MenuButton onClick={() => toggleMenu()}>|||</MenuButton>}
+            <AddressBar name={"toAddress"} onChange={onInputChange} value={toAddress} onBlur={null} />
+            {price && <RequestButton onClick={onAddressSubmit} disabled={toAddress === ""} value={`Request Ride ($${price})`} />}
+            <ExtendedButton onClick={onAddressSubmit} disabled={toAddress === ""} value={price ? "Change Address" : "Pick Address"} />
             <Map ref={mapRef} />
         </Sidebar>
     </Container>
